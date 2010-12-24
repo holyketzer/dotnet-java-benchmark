1f47c788a325907697d9aa9507c36cfe5d744683
holyketzer <holyketzer@9b0458c6-2f29-a3aa-bcc8-13aca7f17889>
2010-12-24 15:09:51 +0000

    ++ Базовая ООП структура для создания тестов в проекте .Net

---
+++ dotNet/RandomTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPerformanceTest
+{
+    class IntRandomTest : SomeTest
+    {
+        public IntRandomTest()
+        {
+            _name = "Генерация случайных чисел int";
+        }
+
+        private Random rnd = new Random();
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 100000000; ++i)
+            {
+                int x = rnd.Next();
+            }
+            StopTiming();            
+        }
+    }
+
+    class DoubleRandomTest : SomeTest
+    {
+        public DoubleRandomTest()
+        {
+            _name = "Генерация случайных чисел double";
+        }
+
+        private Random rnd = new Random();
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 100000000; ++i)
+            {
+                double x = rnd.NextDouble();
+            }
+            StopTiming();
+        }
+    }
+}
---
+++ dotNet/SomeTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPerformanceTest
+{
+    public abstract class SomeTest
+    {
+        public abstract void Do();
+
+        protected string _name = "";
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        protected int _time = 0;
+        public int Time
+        {
+            get { return _time; }
+        }
+
+        protected void StartTiming()
+        {
+            _time = Environment.TickCount;
+        }
+
+        protected void StopTiming()
+        {
+            _time = Environment.TickCount - _time;
+        }
+    }
+}
