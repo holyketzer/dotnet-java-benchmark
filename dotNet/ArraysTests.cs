775689b68fa56cf7ed9855e3d66ae267546fbd1a
holyketzer <holyketzer@9b0458c6-2f29-a3aa-bcc8-13aca7f17889>
2010-12-27 11:30:24 +0000

    ++ .Net Математические тесты ++ .Net Тесты на работу с массивами

---
+++ dotNet/ArraysTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPerformance.ArraysTests
+{
+    class ArrayIntCreateTest : SomeTest
+    {
+        public ArrayIntCreateTest()
+        {
+            _name = "Создние и заполнение одномерного массива int[]";
+        }
+        
+        public override void Do()
+        {
+            StartTiming();
+            int[] array = new int[50000000];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i;
+            }
+            StopTiming();
+        }
+    }
+
+    class ArrayIntAccessTest : SomeTest
+    {
+        public ArrayIntAccessTest()
+        {
+            _name = "Последовательный доступ к элементам одномерного массива int[]";
+        }
+
+        public override void Do()
+        {            
+            int[] array = new int[50000000];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i;
+            }
+
+            StartTiming();
+            for (int i = 0; i < array.Length; ++i)
+            {
+                int x = array[i];
+            }
+            for (int i = array.Length - 1; i > 0; --i)
+            {
+                int x = array[i];
+            }
+            StopTiming();
+        }
+    }
+
+    class ArrayDoubleCreateTest : SomeTest
+    {
+        public ArrayDoubleCreateTest()
+        {
+            _name = "Создние и заполнение одномерного массива double[]";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            double[] array = new double[50000000];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i;
+            }
+            StopTiming();
+        }
+    }
+
+    class ArrayDoubleAccessTest : SomeTest
+    {
+        public ArrayDoubleAccessTest()
+        {
+            _name = "Последовательный доступ к элементам одномерного массива double[]";
+        }
+
+        public override void Do()
+        {
+            double[] array = new double[50000000];
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = i;
+            }
+
+            StartTiming();
+            for (int i = 0; i < array.Length; ++i)
+            {
+                double x = array[i];
+            }
+            for (int i = array.Length - 1; i > 0; --i)
+            {
+                double x = array[i];
+            }
+            StopTiming();
+        }
+    }
+    
+    class ArraysTestsRunner : SomeTestRunner
+    {
+        protected override void InitTests()
+        {
+            _name = "Массивы";
+
+            _tests.Add(new ArrayIntCreateTest());
+            _tests.Add(new ArrayIntAccessTest());
+            _tests.Add(new ArrayDoubleCreateTest());
+            _tests.Add(new ArrayDoubleAccessTest());
+        }
+    }
+}
---
+++ dotNet/MathTests.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPerformance.Math
+{
+    class Div10Test : SomeTest
+    {
+        public Div10Test()
+        {
+            _name = "Деление целых чисел на 10";
+        }
+        
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 1000000000; ++i)
+            {
+                int x = i / 10;
+            }
+            StopTiming();
+        }
+    }
+
+    class SqrtTest : SomeTest
+    {
+        public SqrtTest()
+        {
+            _name = "Квадратный корень";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 1000000000; ++i)
+            {
+                double x = System.Math.Sqrt(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class SinTest : SomeTest
+    {
+        public SinTest()
+        {
+            _name = "Синус";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 1000000000; ++i)
+            {
+                double x = System.Math.Sin(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class CosTest : SomeTest
+    {
+        public CosTest()
+        {
+            _name = "Косинус";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 1000000000; ++i)
+            {
+                double x = System.Math.Cos(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class TanTest : SomeTest
+    {
+        public TanTest()
+        {
+            _name = "Тангенс";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 10000000; ++i)
+            {
+                double x = System.Math.Tan(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class Log10Test : SomeTest
+    {
+        public Log10Test()
+        {
+            _name = "Десятичный логарифм";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 10000000; ++i)
+            {
+                double x = System.Math.Log10(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class ExpTest : SomeTest
+    {
+        public ExpTest()
+        {
+            _name = "Экспонента";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 10000000; ++i)
+            {
+                double x = System.Math.Exp(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class AsinTest : SomeTest
+    {
+        public AsinTest()
+        {
+            _name = "Арксинус";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 10000000; ++i)
+            {
+                double x = System.Math.Asin(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class AcosTest : SomeTest
+    {
+        public AcosTest()
+        {
+            _name = "Арккосинус";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 10000000; ++i)
+            {
+                double x = System.Math.Acos(i);
+            }
+            StopTiming();
+        }
+    }
+
+    class AtanTest : SomeTest
+    {
+        public AtanTest()
+        {
+            _name = "Арктангенс";
+        }
+
+        public override void Do()
+        {
+            StartTiming();
+            for (int i = 0; i < 10000000; ++i)
+            {
+                double x = System.Math.Atan(i);
+            }
+            StopTiming();
+        }
+    }
+    
+    class MathTestsRunner : SomeTestRunner
+    {
+        protected override void InitTests()
+        {
+            _name = "Математические";
+
+            _tests.Add(new Div10Test());
+            _tests.Add(new SqrtTest());
+            _tests.Add(new SinTest());
+            _tests.Add(new CosTest());
+            _tests.Add(new TanTest());
+            _tests.Add(new Log10Test());
+            _tests.Add(new ExpTest());
+            _tests.Add(new AsinTest());
+            _tests.Add(new AcosTest());
+            _tests.Add(new AtanTest());
+        }
+    }
+}
